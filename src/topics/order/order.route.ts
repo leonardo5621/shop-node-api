0e1fd1285c70e0fb7aba8e0be43f1503663f8663
leonardo5621 <leonardo5621@gmail.com>
2022-06-22 19:33:51 -0300

    Update order.route.ts

--- src/topics/order/order.route.ts
+++ src/topics/order/order.route.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { OrderController } from './order.controller';
 import { routerTo } from '../../helpers/router_error_handler';
-import { tokenOrderValidation } from '../../helpers/auth';
 
 const router = express.Router();
 
